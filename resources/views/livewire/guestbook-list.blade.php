5872484c82f4d99bd167ccf0e03d1bcc7eb631c8
fawaziwalewa <fawaziwalewa@gmail.com>
2023-12-12 14:53:41 +0100

    refactor: update layout

--- resources/views/livewire/guestbook-list.blade.php
+++ resources/views/livewire/guestbook-list.blade.php
@@ -5,17 +5,17 @@
             <div class="splide__list">
                 @foreach ($guestbooks as $guestbook)
                     <div
-                        class="flex flex-col items-stretch justify-between w-full max-w-sm gap-2 px-5 py-3 mx-auto text-center bg-white rounded-lg splide__slide">
+                        class="flex flex-col items-stretch justify-between w-full max-w-sm gap-2 py-3 mx-auto text-center bg-white rounded-lg splide__slide">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                             class="h-16 mx-auto text-gray-400 before:w-16 ">
                             <path fill-rule="evenodd"
                                 d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97-1.94.284-3.916.455-5.922.505a.39.39 0 00-.266.112L8.78 21.53A.75.75 0 017.5 21v-3.955a48.842 48.842 0 01-2.652-.316c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <h3 class="text-3xl font-parisienne text-[#32214d]">{{ $guestbook->name }}</h3>
-                        <div class="text-center text-gray-500 line-clamp-6">{!! $guestbook->message !!}</div>
+                        <h3 class="text-3xl font-parisienne text-[#32214d] px-3">{{ $guestbook->name }}</h3>
+                        <div class="px-3 text-center text-gray-500 line-clamp-6">{!! $guestbook->message !!}</div>
                         <div class="relative">
-                            <div class="flex items-center justify-between">
+                            <div class="flex items-center justify-between px-3">
                                 <div class="py-1 group w-max">
                                     <div
                                         class="absolute z-10 hidden bg-white group-hover:flex bottom-full drop-shadow rounded-3xl reactions">
